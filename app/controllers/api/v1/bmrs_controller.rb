98ea2ac94e86353dca3b2e7415ff2be984fd714f
Satria <satriajanaka09@gmail.com>
2021-08-23 22:25:16 +0700

    add create action and bmr calculation

--- app/controllers/api/v1/bmrs_controller.rb
+++ app/controllers/api/v1/bmrs_controller.rb
@@ -6,4 +6,22 @@ class Api::V1::BmrsController < ApplicationController
         render json: @bmrs
     end
 
+    #Save and calculate BMR
+    def create
+
+        #assign the value from the request body to data_json variable
+        data_json = JSON.parse request.body.read
+
+        #create the new instance from Bmr class
+        @bmr = Bmr.new(data_json)
+
+        #doing the calculation
+        @bmr.bmr_value = 66.47 + (13.75 * @bmr.weight) + (5.003 * @bmr.height) - (6.755 * @bmr.age)
+
+        if @bmr.save
+            render json: @bmr
+        else
+            render json: {error: 'unable to create BMR data'}, status: 400
+        end
+    end
 end
